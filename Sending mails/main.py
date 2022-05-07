098380f8bc5a92df07b3b1d611b76631ab436afa
Sarvjeet Bhardwaj <sjitbh121993@gmail.com>
2022-05-07 20:14:32 +0530

    Hiding credeentials

--- Sending mails/main.py
+++ Sending mails/main.py
@@ -6,7 +6,7 @@ myemail='sjitbh121993@gmail.com'
 
 with smtplib.SMTP('smtp.gmail.com') as connection:
     connection.starttls()
-    connection.login(user=myemail, password='BAMBHOLI')
+    connection.login(user=myemail, password=your_password)
     connection.sendmail(from_addr=myemail, to_addrs='sjitbh@rediffmail.com', msg='Subject:Hello\n\n This is my mail body ')
 
     connection.close()
